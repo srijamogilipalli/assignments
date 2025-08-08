1b8da0650016adc36e3c60441422fb2df06de01f
srija mogilipalli <srijamogilipalli@gmail.com>
2025-08-08 21:13:28 +0530

    Assignments 1

---
+++ src/assignment/Assignment1_DataTypes.java
@@ -0,0 +1,11 @@
+package assignment;
+
+public class Assignment1_DataTypes {
+
+	public static void main(String[] args) {
+		float temperature = 45.6f;
+		System.out.println(temperature);
+
+	}
+
+}
